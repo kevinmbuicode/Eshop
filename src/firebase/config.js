81a63353ac8b284c74f56c049981a3cd71e53ead
kevinmbuicode <kevinmushan@gmail.com>
2022-12-01 16:13:31 +0300

    fix: env not to prod

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,27 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage";
-
-// // Your web app's Firebase configuration
-// export const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FB_API_KEY,
-//   authDomain: "eshop-ea8e7.firebaseapp.com",
-//   projectId: "eshop-ea8e7",
-//   storageBucket: "eshop-ea8e7.appspot.com",
-//   messagingSenderId: "146569443233",
-//   appId: "1:146569443233:web:46db000fc2c897f3b73de0",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
-// export const storage = getStorage(app);
-
-// export default app;
-
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -30,7 +6,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: process.env.NODE_ENV,
+  apiKey: process.env.REACT_APP_FB_KEY,
   authDomain: "eshop-a3598.firebaseapp.com",
   projectId: "eshop-a3598",
   storageBucket: "eshop-a3598.appspot.com",
