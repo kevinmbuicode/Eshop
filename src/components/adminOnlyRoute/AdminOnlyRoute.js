37aba4c48073e921250abc8ace68058b70bcde53
kevinmbuicode <kevinmushan@gmail.com>
2022-12-01 14:04:46 +0300

    fix: change admin email

--- src/components/adminOnlyRoute/AdminOnlyRoute.js
+++ src/components/adminOnlyRoute/AdminOnlyRoute.js
@@ -6,7 +6,7 @@ import { selectEmail } from "../../redux/slice/authSlice";
 const AdminOnlyRoute = ({ children }) => {
   const userEmail = useSelector(selectEmail);
 
-  if (userEmail === "testapp@gmail.com") {
+  if (userEmail === "ftbuapplication@gmail.com") {
     return children;
   }
   return (
@@ -26,7 +26,7 @@ const AdminOnlyRoute = ({ children }) => {
 export const AdminOnlyLink = ({ children }) => {
   const userEmail = useSelector(selectEmail);
 
-  if (userEmail === "testapp@gmail.com") {
+  if (userEmail === "ftbuapplication@gmail.com") {
     return children;
   }
   return null;
